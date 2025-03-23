68b361f8368ceaf4be378a07948804ca10f28c61
Josue Eliezer Gomez Soto <jsegomez06@gmail.com>
2025-03-22 21:25:50 -0600

    clase 3 completada

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, inject, signal, OnInit, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
+import { ProductComponent } from '@products/components/product/product.component';
+
+import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
+
+@Component({
+    selector: 'app-list',
+    imports: [CommonModule, ProductComponent, RouterLinkWithHref],
+    templateUrl: './list.component.html'
+})
+export default class ListComponent implements OnInit, OnChanges {
+
+  products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
+  private cartService = inject(CartService);
+  private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
+
+  ngOnInit() {
+    this.getCategories();
+  }
+
+  ngOnChanges() {
+    this.getProducts();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product)
+  }
+
+  private getProducts() {
+    this.productService.getProducts(this.category_id)
+    .subscribe({
+      next: (products) => {
+        this.products.set(products);
+      }
+    })
+  }
+
+  private getCategories() {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      }
+    })
+  }
+}
---
+++ src/app/domains/shared/directives/highlight.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef, inject, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+  standalone: true
+})
+export class HighlightDirective implements OnInit {
+
+  element = inject(ElementRef);
+
+  ngOnInit() {
+    this.element.nativeElement.style.backgroundColor = "red";
+  }
+
+}
