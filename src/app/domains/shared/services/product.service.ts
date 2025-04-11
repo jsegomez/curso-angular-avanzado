b1515070afb272a3f4f4c8adea23649e5a09e214
jsegomez <jsegomez06@gmail.com>
2025-04-11 17:43:11 -0600

    class 9 completed

--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -9,11 +9,13 @@ import { environment } from 'src/environments/environment.development';
 export class ProductService {
   private http = inject(HttpClient);
 
-  getProducts(category_id?: string) {
+  getProducts(params: { categoryId?: string, categorySlug?: string } ) {
     const url = new URL(`${environment.apiUrl}/api/v1/products`);
-    if (category_id) {
-      url.searchParams.set('categoryId', category_id);
-    }
+    const { categoryId: category, categorySlug: slug } = params;
+
+    if (category) url.searchParams.set('categoryId', category);
+    if (slug) url.searchParams.set('categorySlug', slug);
+
     return this.http.get<Product[]>(url.toString());
   }
 
