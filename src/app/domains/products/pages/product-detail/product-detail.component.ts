436f9421db392009cd75388e211deb2d5ed2cc17
jsegomez <jsegomez06@gmail.com>
2025-04-14 16:54:05 -0600

    class 13 completed

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit, input } from '@angular/core';
+import { Component, inject, signal, OnInit, input, linkedSignal } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { ProductService } from '@shared/services/product.service';
 import { Product } from '@shared/models/product.model';
@@ -15,7 +15,14 @@ import { CartService } from '@shared/services/cart.service';
 export default class ProductDetailComponent implements OnInit {
   readonly id = input<string>();
   product = signal<Product | null>(null);
-  cover = signal('');
+  cover = linkedSignal({
+    source: this.product,
+    computation: (product, previousVal) => {
+      if (product && product.images.length > 0) return product.images[0];
+      else return previousVal?.value;
+    }
+  });
+
   private productService = inject(ProductService);
   private cartService = inject(CartService);
 
@@ -23,20 +30,12 @@ export default class ProductDetailComponent implements OnInit {
     const id = this.id();
     if (id) {
       this.productService.getOne(id).subscribe({
-        next: (product) => {
-          this.product.set(product);
-          if (product.images.length > 0) {
-            this.cover.set(product.images[0]);
-          }
-        },
+        next: (product) => this.product.set(product),
       });
     }
   }
 
-  changeCover(newImg: string) {
-    this.cover.set(newImg);
-  }
-
+  
   addToCart() {
     const product = this.product();
     if (product) {
